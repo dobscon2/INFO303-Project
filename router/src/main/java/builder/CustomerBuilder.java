ce47e4af6aaad1672df8417a19a7ce1bcf479af1
Connor Dobson <dobco402@student.otago.ac.nz>
2021-05-21 13:26:30 +1200

    Creating CustomerBuilder class for Camel Router

---
+++ router/src/main/java/builder/CustomerBuilder.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package builder;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.ExchangePattern;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
+
+/**
+ *
+ * @author cdobson
+ */
+
+public class CustomerBuilder extends RouterBuilder {
+    
+    @Override
+    public void configure() throw Exception {
+        
+    }
+    
+}
